1dabb8e851349d4e89e36e0ca411f02bfaad195e
spoyodevelop <kjy9505@gmail.com>
2026-09-13 13:21:14 +0900

    fix: import fix

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,12 @@
 import { KoreaMap } from "./KoreaMap";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <>
-      <KoreaMap />;
-      <SpeedInsights />;
+      <KoreaMap />
+      <SpeedInsights />
       <Analytics />
     </>
   );
